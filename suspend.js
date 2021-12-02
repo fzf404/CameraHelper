89dd956fdf0357fd86d082772389610756bb73db
你们的小f <nmdfzf404@163.com>
2021-12-02 21:18:15 +0800

    🙌 增加视频镜像功能

--- suspend.js
+++ suspend.js
@@ -1,34 +1,43 @@
-let showLabel = document.querySelector('.show');
+let showLabel = document.getElementById("video");
+
 // 默认窗口大小
 let showWidth = 50;
+
+// 是否镜像
+localStorage["mirror"] == "true" ? showLabel.classList.add("mirror") : null;
+
 // 相机id
 let showOpt = {
   audio: false,
   video: {
-    optional: [{
-      // 获取参数
-      sourceId: localStorage['mediaKey']
-    }]
+    optional: [
+      {
+        // 获取参数
+        sourceId: localStorage["mediaKey"],
+      },
+    ],
   },
-}
+};
 
+// 展示
 let showVideo = () => {
-  navigator.mediaDevices.getUserMedia(showOpt)
-    .then(mediaStream => {
+  navigator.mediaDevices
+    .getUserMedia(showOpt)
+    .then((mediaStream) => {
       showLabel.srcObject = mediaStream;
-      showLabel.onloadedmetadata = e => {
+      showLabel.onloadedmetadata = (e) => {
         showLabel.play();
       };
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(`${err.name}:${err.message}`);
     });
-}
+};
 
 showVideo();
 
 // 监听按键
-document.addEventListener("keydown", event => {
+document.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "Escape":
       window.close();
@@ -40,10 +49,10 @@ document.addEventListener("keydown", event => {
       showLabel.style.width = `${showWidth}vw`;
       break;
     case "=":
-      if (showWidth < 100) { 
+      if (showWidth < 100) {
         showWidth += 5;
       }
       showLabel.style.width = `${showWidth}vw`;
       break;
   }
-})
+});
--- video.js
+++ video.js
@@ -1,11 +1,12 @@
 // 展示用标签
-let showLabel = document.querySelector('.show');
-let recordLabel = document.querySelector('a');
-let choiceLabel = document.querySelector('.choice');
+let showLabel = document.getElementById("video"); // 视频
+let recordDownload = document.querySelector("a"); // 下载
+let choiceLabel = document.getElementById("choice"); // 摄像头选择
+let recordButton = document.getElementById("record"); // 录像按钮
 // 拍照绘图
-let canvas = document.createElement('canvas');
-let context = canvas.getContext('2d');
-// 录像
+let canvas = document.createElement("canvas");
+let context = canvas.getContext("2d");
+// 录制对象
 let recorder;
 
 // 默认配置
@@ -13,42 +14,53 @@ let showOpt = {
   audio: false,
   video: true,
 };
+
+// 摄像头是否关闭
 let flag = true;
+// 是否开启镜像
+let mirror = false;
+// 是否有媒体设备
 let hasMedia = false;
 
 // 获取媒体设备列表
 let getMedia = () => {
-  navigator.mediaDevices.enumerateDevices()
-    .then(devices => {
+  navigator.mediaDevices
+    .enumerateDevices()
+    .then((devices) => {
       devices.forEach((device) => {
         // 判断设备是否为视频类型
         if (device.kind === "videoinput") {
           // 增加select标签
-          choiceLabel.insertAdjacentHTML('beforeend', `<option value="${device.deviceId}">${device.label}</option>`);
+          choiceLabel.insertAdjacentHTML(
+            "beforeend",
+            `<option value="${device.deviceId}">${device.label}</option>`
+          );
           hasMedia = true;
         }
       });
-      if(hasMedia == false){
-        alert('没有找到相机设备');
+      if (hasMedia == false) {
+        alert("没有找到相机设备");
       }
-    }).catch(err => {
-      console.log(`${err.name}:${err.message}`);
     })
-}
+    .catch((err) => {
+      console.log(`${err.name}:${err.message}`);
+    });
+};
 
 // 展示
 let showVideo = () => {
-  navigator.mediaDevices.getUserMedia(showOpt)
-    .then(mediaStream => {
+  navigator.mediaDevices
+    .getUserMedia(showOpt)
+    .then((mediaStream) => {
       showLabel.srcObject = mediaStream;
-      showLabel.onloadedmetadata = e => {
+      showLabel.onloadedmetadata = (e) => {
         showLabel.play();
       };
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(`${err.name}:${err.message}`);
     });
-}
+};
 
 // 切换摄像头
 let chVideo = () => {
@@ -57,13 +69,15 @@ let chVideo = () => {
   showOpt = {
     audio: false,
     video: {
-      optional: [{
-        sourceId: deviceId
-      }]
+      optional: [
+        {
+          sourceId: deviceId,
+        },
+      ],
     },
-  }
+  };
   showVideo();
-}
+};
 
 // 拍照
 let takePhoto = () => {
@@ -73,61 +87,71 @@ let takePhoto = () => {
   //利用video画图
   context.drawImage(showLabel, 0, 0);
   //将canvas图像转换为base64字符串，设置格式以及压缩比例
-  let image = canvas.toDataURL('image/jpeg', 90 / 100);
+  let image = canvas.toDataURL("image/jpeg", 90 / 100);
 
   // 下载标签更新并点击
-  recordLabel.href = image;
-  recordLabel.click();
-  return image
-}
+  recordDownload.href = image;
+  recordDownload.click();
+  return image;
+};
 
 // 录像
-let recordVideo = () => {
+let startRecord = () => {
   // 转换Opt为局部变量
   let recordOpt = showOpt;
   // 开启音频录制
   recordOpt.audio = true;
-  console.log('start');
+  console.log("start");
   // 获取音视频流
-  navigator.mediaDevices.getUserMedia(recordOpt)
-    .then(mediaStream => {
+  navigator.mediaDevices
+    .getUserMedia(recordOpt)
+    .then((mediaStream) => {
       // 初始化记录对象
       recorder = new MediaRecorder(mediaStream);
-      recorder.ondataavailable = e => {
+      recorder.ondataavailable = (e) => {
         let recordData = e.data;
-        recordLabel.href = URL.createObjectURL(recordData);
+        recordDownload.href = URL.createObjectURL(recordData);
       };
       recorder.start();
-      // 按钮变色
-      document.getElementsByClassName("start")[0].classList.add('disable');
-      document.getElementsByClassName("stop")[0].classList.remove('disable');
-
+      // 按钮更改内容
+      recordButton.onclick = stopRecord;
+      recordButton.classList.replace("btn-b", "btn-o");
+      recordButton.innerHTML = "停止";
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(`${err.name}:${err.message}`);
     });
-}
+};
 
 // 停止录制
-let stopVideo = () => {
+let stopRecord = () => {
   recorder.stop();
-  console.log('stop!');
-  // 修改按钮颜色
-  document.getElementsByClassName("stop")[0].classList.add('disable');
-  document.getElementsByClassName("start")[0].classList.remove('disable');
+  console.log("stop!");
+  // 按钮更改内容
+  recordButton.onclick = startRecord;
+  recordButton.classList.replace("btn-o", "btn-b");
+  recordButton.innerHTML = "录制";
   // 延时加载
-  setTimeout(
-    () => recordLabel.click(),
-    1000
-  )
-}
+  setTimeout(() => recordDownload.click(), 1000);
+};
+
+// 镜像视频
+let mirrorVideo = () => {
+  mirror
+    ? showLabel.classList.remove("mirror")
+    : showLabel.classList.add("mirror");
+
+  mirror = !mirror;
+};
 
 // 悬浮
-function suspend() {
+let suspend = () => {
   // 传入摄像头id
-  localStorage.setItem('mediaKey', choiceLabel.value);
-  utools.createBrowserWindow('suspend.html', {
-    title: 'camera',
+  localStorage.setItem("mediaKey", choiceLabel.value);
+  // 传入镜像情况
+  localStorage.setItem("mirror", mirror);
+  utools.createBrowserWindow("suspend.html", {
+    title: "camera",
     width: parseInt(showLabel.videoWidth),
     height: parseInt(showLabel.videoHeight),
     useContentSize: true,
@@ -137,29 +161,30 @@ function suspend() {
     fullscreenable: false,
     //背景透明，防止放大缩小时出现白框
     transparent: true,
-    backgroundColor: '#00000000',
+    backgroundColor: "#00000000",
     frame: false,
     alwaysOnTop: true,
   });
-}
+};
 
 // 启动
 let start = () => {
   showVideo();
   flag = true;
-}
+};
 
 // 退出
 let exit = () => {
   if (flag == true) {
+    // 遍历并停止
     showLabel.srcObject.getTracks().forEach((v) => {
       v.stop();
-    })
+    });
     flag = false;
   } else {
     start();
   }
-}
+};
 
 getMedia();
 start();
